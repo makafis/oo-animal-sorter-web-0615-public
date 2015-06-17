89b340dece3244fd63e7219a7106c3a0e05ea3b8
makafis <vinnyalfieri@gmail.com>
2015-06-17 15:42:27 -0400

    complete

--- lib/animal_sorter.rb
+++ lib/animal_sorter.rb
@@ -12,3 +12,30 @@
 # should include the sea creatures, the second, land animals.
 
 # Read the test suite for an example of a nested array.
+require 'pry'
+class AnimalSorter
+SEA_ANIMALS = ["marlin", "octopus", "fish"]
+LAND_ANIMALS = ["aardvark", "cat", "elephant"]
+
+def initialize(array)
+  @split_array = array.each do |value|
+    value
+  end
+end
+
+
+def to_a
+  @return_array = [[],[]]
+  @split_array.each do |value|
+    if SEA_ANIMALS.include?(value)
+      @return_array[0] << value
+    elsif LAND_ANIMALS.include?(value)
+      @return_array[1] << value
+    end
+  end
+  @return_array
+end
+
+
+
+end
